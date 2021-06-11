9c7756f6a7c138fa80e71f44847481f8ba738b8d
TSUYUKI kohei <kotsuyuki@gmail.com>
2021-06-11 22:29:45 +0900

    ABC048A, B を精進

---
+++ kotlin/abc/abc048/Abc048A.kt
@@ -0,0 +1,7 @@
+package abc.abc048
+
+fun main() {
+    val axc = readLine()!!.split(" ")
+    val init = axc[1].first()
+    println("A${init}C")
+}
---
+++ kotlin/abc/abc048/Abc048B.kt
@@ -0,0 +1,8 @@
+package abc.abc048
+
+fun main() {
+    val (a, b, x) = readLine()!!.split(" ").map(String::toLong)
+    val fb = b / x + 1
+    val fa = if (a > 0) (a - 1) / x + 1 else 0
+    println(fb - fa)
+}
