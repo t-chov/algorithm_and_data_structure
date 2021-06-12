a1993dd99b60d9a30e3a1bb64fadd32bd46044c0
TSUYUKI kohei <kotsuyuki@gmail.com>
2021-06-12 11:10:58 +0900

    ABC049 を精進

---
+++ kotlin/abc/abc049/Abc049B.kt
@@ -0,0 +1,10 @@
+package abc.abc049
+
+fun main() {
+    val (H, W) = readLine()!!.split(" ").map(String::toInt)
+    val pics = List(H) { readLine()!! }
+    pics.forEach {
+        println(it)
+        println(it)
+    }
+}
