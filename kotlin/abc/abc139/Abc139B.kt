32f8695f01c2f2863e9efb2823ada73cc78323b5
TSUYUKI kohei <kotsuyuki@gmail.com>
2021-06-19 11:15:07 +0900

    ABC056 を精進

---
+++ kotlin/abc/abc139/Abc139B.kt
@@ -0,0 +1,12 @@
+package abc.abc139
+
+fun main() {
+    val (a, b) = readLine()!!.split(" ").map(String::toInt)
+    var sum = 1
+    var taps = 0
+    while (sum < b) {
+        sum = sum - 1 + a
+        taps ++
+    }
+    println(taps)
+}
