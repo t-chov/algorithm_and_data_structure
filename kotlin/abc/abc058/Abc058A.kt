f6b974fa5e07c3e1341ac89c4d75846d667f9897
TSUYUKI kohei <kotsuyuki@gmail.com>
2021-06-19 18:11:55 +0900

    ABC058 を精進

---
+++ kotlin/abc/abc058/Abc058A.kt
@@ -0,0 +1,10 @@
+package abc.abc058
+
+fun main() {
+    val pillars = readLine()!!.split(" ").map(String::toInt)
+    if (pillars[1] - pillars[0] == pillars[2] - pillars[1]) {
+        println("YES")
+    } else {
+        println("NO")
+    }
+}
---
+++ kotlin/abc/abc058/Abc058C.kt
@@ -0,0 +1,13 @@
+package abc.abc058
+
+fun main() {
+    val n = readLine()!!.toInt()
+    val strings = List(n) { readLine()!! }
+    val bunpu = ('a'..'z').map { c ->
+        val cnt = strings.map { s -> s.count { it == c} }.min() ?: 0
+        c to cnt
+    }
+    bunpu.forEach { b ->
+        (0 until b.second).forEach { print(b.first) }
+    }
+}
