b85376cc0e8c036387a3392965c2c0b9b3c51221
TSUYUKI kohei <kotsuyuki@gmail.com>
2021-06-21 21:33:27 +0900

    ABC065C

---
+++ kotlin/abc/abc065/Abc065C.kt
@@ -0,0 +1,28 @@
+package abc.abc065
+
+import kotlin.math.absoluteValue
+
+fun main() {
+    val (N, M) = readLine()!!.split(" ").map(String::toLong)
+    val mod = 1_000_000_007L
+    if ((N - M).absoluteValue > 1L) {
+        println(0)
+        return
+    }
+    var n = N
+    for (i in 1L until n) {
+        n = (n * (N - i)) % mod
+    }
+
+    var m = M
+    for (i in 1L until m) {
+        m = (m * (M - i)) % mod
+    }
+
+    // println("$n $m")
+    if (N == M) {
+        println((n * m * 2) % mod)
+        return
+    }
+    println((n * m) % mod)
+}
