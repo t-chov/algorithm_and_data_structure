7f4d8bde6bfd09058364f9b778c5dbdc23d5d9e3
TSUYUKI kohei <kotsuyuki@gmail.com>
2021-07-01 20:49:47 +0900

    ABC076AB

---
+++ kotlin/abc/abc076/Abc076A.kt
@@ -0,0 +1,15 @@
+package abc.abc076
+
+fun main() {
+    val r = readLine()!!.toDouble()
+    val g = readLine()!!.toDouble()
+    // (r + next) / 2 = g
+    // r + next = 2g
+    // next = 2g - r
+    val next = 2 * g - r
+    if (next % 1.0 == 0.0) {
+        println(next.toInt())
+    } else {
+        println(next)
+    }
+}
