185b75b6dc84df6d3c9f8231b785efb6366043a5
TSUYUKI kohei <kotsuyuki@gmail.com>
2021-07-08 22:20:10 +0900

    Abc082C, ABC120D

---
+++ kotlin/abc/abc082/Abc082C.kt
@@ -0,0 +1,16 @@
+package abc.abc082
+
+fun main() {
+    val n = readLine()!!.toInt()
+    val a = HashMap<Int, Int>()
+    readLine()!!.split(" ").forEach { ais ->
+        val ai = ais.toInt()
+        a[ai] = (a[ai] ?: 0) + 1
+    }
+    var ans = 0L
+    a.forEach { i, cnt ->
+        if (cnt < i) ans += cnt
+        else ans += cnt - i
+    }
+    println(ans)
+}
--- kotlin/abc/abc120/Abc120D.kt
+++ kotlin/abc/abc120/Abc120D.kt
@@ -1,3 +1,7 @@
+package abc.abc120
+
+import java.io.PrintWriter
+
 private class UnionFind(
         // 各頂点の親頂点の番号.
         private val roots: Array<Int>,
@@ -38,4 +42,28 @@ private class UnionFind(
             )
         }
     }
+}
+
+private fun main() {
+    val pw = PrintWriter(System.out)
+    val (n, m) = readLine()!!.split(" ").map(String::toInt)
+    val uf = UnionFind.initialize(n)
+    val edges = List(m) { readLine()!!.split(" ").map(String::toInt).let{ it[0] - 1 to it[1] - 1 }}
+    val ans = Array(m) { n.toLong() * (n.toLong() - 1L) / 2L }
+    val cnt = Array(n) { 1L }
+    for (i in m - 1 downTo 1) {
+        val (a, b) = edges[i]
+        if (uf.root(a) == uf.root(b)) {
+            ans[i - 1] = ans[i]
+        } else {
+            ans[i - 1] = ans[i] - cnt[uf.root(a)] * cnt[uf.root(b)]
+            val sm = cnt[uf.root(a)] + cnt[uf.root(b)]
+            uf.unite(a, b)
+            cnt[uf.root(a)] = sm
+        }
+    }
+    ans.forEach { a ->
+        pw.println(a)
+    }
+    pw.flush()
 }
